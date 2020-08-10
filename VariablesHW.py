705d61f318da3af4052e79ec7424e58c25fd71f9
ElifUnal <elifunal340@anadolu.edu.tr>
2020-08-10 13:43:15 +0300

    Add files via upload
    Variables Answers

---
+++ VariablesHW.py
@@ -0,0 +1,90 @@
+{
+ "cells": [
+  {
+   "cell_type": "raw",
+   "metadata": {},
+   "source": [
+    "# 1. Define two variables as x and y. Assign 1 to x and '1' to y. Then check if two variables have the same value and type."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Variables are not the same type and values\n"
+     ]
+    }
+   ],
+   "source": [
+    "x=1\n",
+    "y='1'\n",
+    "if x==y:\n",
+    "    print(\"Variables the same type and values\")\n",
+    "else:\n",
+    "    print(\"Variables are not the same type and values\")"
+   ]
+  },
+  {
+   "cell_type": "raw",
+   "metadata": {},
+   "source": [
+    "# 2. Change the type of variable y and make the two variables the same."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Variables the same type and values\n"
+     ]
+    }
+   ],
+   "source": [
+    "x=1\n",
+    "y=1\n",
+    "if x==y:\n",
+    "    print(\"Variables the same type and values\")\n",
+    "else:\n",
+    "    print(\"Variables are not the same type and values\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
